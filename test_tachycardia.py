b902aefe20587441ac2d785b548d1adaae304265
Huisi Cai <sylvia@HuisimatoMacBook-Pro.local>
2019-02-05 17:18:26 -0500

    Format for pep8: if cond is True

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -5,7 +5,7 @@ def test_is_tachycardic():
     from tachycardia import is_tachycardic
 
     result = is_tachycardic("TAChycardic")
-    assert result == True
+    assert result is True
 
 
 @pytest.mark.parametrize("test_list, expected", [("tachycardic", True),
